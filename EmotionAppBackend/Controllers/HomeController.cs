481ceed255e059f57cdfc5295fc196d49baf8ad3
lyq <1661898579@qq.com>
2025-02-27 14:16:40 +0800

    初始化框架

---
+++ EmotionAppBackend/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+[ApiController]
+[Route("/")]
+public class HomeController : Controller
+{
+    // 处理 GET 请求，返回视图
+    [HttpGet]
+    [Route("test")]
+    public async Task<Result> Test()
+    {
+        await Task.Delay(1010);
+        return new Result(233);
+    }
+    public record Result(int value);
+}
---
+++ EmotionAppBackend/Program.cs
@@ -0,0 +1,54 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 注册 MVC 控制器服务，自动扫描所有 Controller（需放在项目中符合约定的 Controller 类）
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    
+    
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+// 使用 MapControllers()，让框架自动根据控制器上的 [Route] 属性配置路由
+app.MapControllers();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
