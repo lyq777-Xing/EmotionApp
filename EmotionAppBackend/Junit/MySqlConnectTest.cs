cb1d64d95a64965a107feabce2ec48d5749ea004
lyq <1661898579@qq.com>
2025-02-27 15:27:06 +0800

    MysqlConnect

---
+++ EmotionAppBackend/Junit/MySqlConnectTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+
+[TestClass]
+public class MathUtilsTests
+{
+    [TestMethod]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        string connectionString = "server=localhost;user=root;password=lyq;database=EmotionApp;";
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to MySQL!");
+
+                // 示例：执行查询
+                string query = "SELECT * FROM user;";
+                MySqlCommand command = new MySqlCommand(query, connection);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["user_name"].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
