232dcaf79d71c36b4ee0f4757ffdd49a2f66f112
lyq <liyqqq@outlook.com>
2025-04-12 01:49:42 +0800

    fix: models

---
+++ Algorithm/HuggingFace/HuggingFace/test1.py
@@ -0,0 +1,12 @@
+from transformers import TrainingArguments
+
+args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=3,
+    weight_decay=0.01,
+)
+
+print("OK ✅")
