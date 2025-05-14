7771f582d99477c54b390b51bf8ad2a640c88772
lyq <liyqqq@outlook.com>
2025-05-14 13:34:47 +0800

    fix: remove ODBC secret

--- EmotionAppBackend/Data/EmotionAppContext.cs
+++ EmotionAppBackend/Data/EmotionAppContext.cs
@@ -28,14 +28,6 @@ public partial class EmotionAppContext : DbContext
 
     public DbSet<EmotionKnowledge> EmotionKnowledgeBase { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        =>
-        optionsBuilder.UseMySql(
-            "server=118.31.55.155;user=root;password=lyq;database=EmotionApp",
-            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql")
-        );
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("utf8mb4_0900_ai_ci").HasCharSet("utf8mb4");
